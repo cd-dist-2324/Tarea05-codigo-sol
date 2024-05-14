2a6d53338fde0512fdfc6fcc1948c92e0c1a7145
cd-dist-2324 <cd.dist.2324@gmail.com>
2024-05-14 20:31:43 +0200

    Modificaciones tarea05

--- LineaArticulo.java
+++ LineaArticulo.java
@@ -10,4 +10,46 @@ public class LineaArticulo {
     private Articulo articulo;
     private CarritoCompra carrito;
 
+
+    
+    public Integer getId() {
+      return id;
+    }
+    public void setId(Integer id) {
+      this.id = id;
+    }
+    public Integer getCantidad() {
+      return cantidad;
+    }
+    public void setCantidad(Integer cantidad) {
+      this.cantidad = cantidad;
+    }
+    public Float getPrecio() {
+      return precio;
+    }
+    public void setPrecio(Float precio) {
+      this.precio = precio;
+    }
+    public Pedido getPedido() {
+      return pedido;
+    }
+    public void setPedido(Pedido pedido) {
+      this.pedido = pedido;
+    }
+    public Articulo getArticulo() {
+      return articulo;
+    }
+    public void setArticulo(Articulo articulo) {
+      this.articulo = articulo;
+    }
+    public CarritoCompra getCarrito() {
+      return carrito;
+    }
+    public void setCarrito(CarritoCompra carrito) {
+      this.carrito = carrito;
+    }
+
+
+    
+
 }
--- Pedido.java
+++ Pedido.java
@@ -1,25 +1,86 @@
+import java.time.LocalDateTime;
 import java.util.Vector;
 
 public class Pedido {
 
   private Integer numero;
 
-  private DateTime fecha;
+  private LocalDateTime fecha;
 
   private Float total;
 
   private EstadosPedido estado;
 
-    /**
+  /**
    * 
    * @element-type LineaArticulo
    */
-  private Vector  lineas;
-    private Comprador comprador;
-    private Direccion direccionEntrega;
+  private Vector<LineaArticulo> lineas;
+  private Comprador comprador;
+  private Direccion direccionEntrega;
+
+  public Integer getNumero() {
+    return numero;
+  }
+
+  public void setNumero(Integer numero) {
+    this.numero = numero;
+  }
+
+  public LocalDateTime getFecha() {
+    return fecha;
+  }
+
+  public void setFecha(LocalDateTime fecha) {
+    this.fecha = fecha;
+  }
+
+  public Float getTotal() {
+    return total;
+  }
+
+  public void setTotal(Float total) {
+    this.total = total;
+  }
+
+  public EstadosPedido getEstado() {
+    return estado;
+  }
+
+  public void setEstado(EstadosPedido estado) {
+    this.estado = estado;
+  }
+
+  public Vector<LineaArticulo> getLineas() {
+    return lineas;
+  }
+
+  public void setLineas(Vector<LineaArticulo> lineas) {
+    this.lineas = lineas;
+  }
+
+  public Comprador getComprador() {
+    return comprador;
+  }
+
+  public void setComprador(Comprador comprador) {
+    this.comprador = comprador;
+  }
+
+  public Direccion getDireccionEntrega() {
+    return direccionEntrega;
+  }
+
+  public void setDireccionEntrega(Direccion direccionEntrega) {
+    this.direccionEntrega = direccionEntrega;
+  }
 
   public Float calcularTotal() {
-  return null;
+    float total = 0;
+    for (LineaArticulo lineaArticulo : lineas) {
+      total += lineaArticulo.getCantidad() * lineaArticulo.getPrecio();
+    }
+    return total;
   }
 
 }
